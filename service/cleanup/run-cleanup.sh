99dbe6aaf930ffe2e800bf0580cec6c382b83bca
Leon Wilms <leonwilms.wk@gmail.com>
2026-07-12 03:04:42 +0200

    adjusted cron job to also delete pcap files which flooded my vm
    (cherry picked from commit 682887f31239cce55cd10a72914fd91aecd000a5)

--- service/cleanup/run-cleanup.sh
+++ service/cleanup/run-cleanup.sh
@@ -9,6 +9,23 @@ if [ -f /app/cleanup.env ]; then
     export SIGNMEMAYBE_NOTARY_VAULT_ROOT
     export SIGNMEMAYBE_MAX_UPLOAD_BYTES
     export SIGNMEMAYBE_CLEANUP_RETENTION_SECONDS
+    export SIGNMEMAYBE_PCAP_ROOT
+    export SIGNMEMAYBE_PCAP_RETENTION_MINUTES
 fi
 
 dotnet /app/SignMeMaybe.dll --cleanup-once
+
+pcap_retention_minutes="${SIGNMEMAYBE_PCAP_RETENTION_MINUTES:-30}"
+case "$pcap_retention_minutes" in
+    ''|*[!0-9]*)
+        pcap_retention_minutes=30
+        ;;
+esac
+
+if [ "$pcap_retention_minutes" -le 0 ]; then
+    pcap_retention_minutes=30
+fi
+
+if [ -n "${SIGNMEMAYBE_PCAP_ROOT:-}" ] && [ -d "$SIGNMEMAYBE_PCAP_ROOT" ]; then
+    find "$SIGNMEMAYBE_PCAP_ROOT" -type f \( -name '*.pcap' -o -name '*.pcapng' -o -name '*.pcap.gz' \) -mmin +"$pcap_retention_minutes" -delete
+fi
